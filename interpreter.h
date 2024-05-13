bdc2555507221a077e42e1c5e4c31ff24634fe5e
Brian <huynh.brian66@gmail.com>
2024-05-13 10:13:33 -0400

    Added kernel and partial implementation of exec function

--- interpreter.h
+++ interpreter.h
@@ -1,9 +1,13 @@
 #ifndef INTERPRETER_H
+#define INTERPRETER_H
+
+#include <iostream>
 #include <vector>
 #include <iomanip>
 #include <sstream>
 #include <fstream>
 #include "shellmemory.h"
+#include "kernel.h"
 
 ListNode shellMemory;
 
@@ -14,10 +18,13 @@ void help() {
      * Even entries contains commands. Odd entries contains Descriptions.
      */
     const std::vector<std::string> commands = {
-        "COMMAND", "DESCRIPTION",
-        "help", "Displays all the commands", "quit", "Exits the shell",
-        "set VAR STRING", "Assigns a value to shell memory", "print Var",
-        "Prints the String assigned to VAR", "run SCRIPT.txt", "Executes the file SCRIPT.txt"
+            "COMMAND", "DESCRIPTION",
+            "help", "Displays all the commands",
+            "quit", "Exits the shell",
+            "set VAR STRING", "Assigns a value to shell memory",
+            "print Var", "Prints the String assigned to VAR",
+            "run SCRIPT.txt", "Executes the file SCRIPT.txt",
+            "exec p1 p2 p3", "Executes concurrent programs. Example exec p1.txt p2.txt"
     };
     const char separator = ' ';
     const int padding = 30;
@@ -32,8 +39,7 @@ void help() {
 }
 
 void setValue(std::string &varName, std::string &value) {
-     shellMemory.add(varName, value);
-     shellMemory.printList();
+    shellMemory.add(varName, value);
 }
 
 void printVariable(const std::string &varName) {
@@ -45,6 +51,7 @@ void run(std::string &fileLocation, bool &signal) {
     testScript.open(fileLocation);
     if (!testScript.is_open()) {
         std::cout << "Script not found" << std::endl;
+        return;
     }
     std::string line;
     while (getline(testScript, line)) {
@@ -52,6 +59,31 @@ void run(std::string &fileLocation, bool &signal) {
         if (line == "quit") { break; }
     }
     testScript.close();
+}
+
+void exec(const std::vector<std::string>& argu, bool &signal) {
+    //Here exec will call the run function
+    //probably best to pass in the array then have it call
+    //run in a loop
+
+    //need to check for valid file extension and if the program has already ran
+    //check if the last 4 characters are a .txt
+    const size_t len = argu.size();
+    for (int i = 1; i < len; ++i) {
+        //std::cout << x << std::endl;
+        std::string ext;
+        size_t lenWord = argu[i].size();
+        for (int j = 0; j < 4; ++j) {
+            ext += argu[i][lenWord - j - 1];
+        }
+        if (ext != "txt.") {
+            std::cout << "Invalid file extension" << std::endl;
+            signal = false;
+        }
+        std::cout << ext << std::endl;
+    }
+    std::vector<std::string> p;
+    myInit(p);
 }
 
 void interpreter(const std::string &command, bool &signal) {
@@ -68,16 +100,24 @@ void interpreter(const std::string &command, bool &signal) {
 
     if (instructions[0] == "help" && argument == 1) {
         help();
-    } else if (instructions[0] == "set" && argument == 3) {
+    }
+    else if (instructions[0] == "set" && argument == 3) {
         setValue(instructions[1], instructions[2]);
-    } else if (instructions[0] == "print" && argument == 2) {
+    }
+    else if (instructions[0] == "print" && argument == 2) {
         printVariable(instructions[1]);
-    } else if (instructions[0] == "run" && argument == 2) {
+    }
+    else if (instructions[0] == "run" && argument == 2) {
         run(instructions[1], signal);
-    } else if (instructions[0] == "quit" && argument == 1) {
+    }
+    else if (instructions[0] == "exec" && argument >=2 && argument <= 4) {
+        exec(instructions, signal);
+    }
+    else if (instructions[0] == "quit" && argument == 1) {
         signal = false;
         std::cout << "Bye!\n";
-    } else {
+    }
+    else {
         std::cout << "Unknown Command\n";
     }
 }
---
+++ kernel.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+#include "interpreter.h"
+#include "shell.h"
+#include "pcb.h"
+#include "ram.h"
+#include "cpu.h"
+
+void addToReady(PCB p);
+
+void myInit(std::vector<std::string>& instructions) {
+    for (int i = 1; i < instructions.size(); ++i) {
+        std::shared_ptr<std::ifstream> program = std::make_shared<std::ifstream>(std::ifstream (instructions[i]));
+        addToRam(program);
+        //auto gg = makePCB(instructions[i]);
+    }
+    std::cout << "Hello the exec calls is here" << std::endl;
+    printfile();
+}
+
+void addToReady(PCB p) {
+
+}
+
+
+int main() {
+    std::cout << "Welcome to the C++ Shell\n";
+    std::cout << "Version 1.0 Created March 2024\n";
+    parse();
+
+    return 0;
+
+}
+
