43421cac372c14a3bb09989a57bbf09f79661a3a
Zynnia <huynh.brian66@gmail.com>
2024-08-25 11:18:27 -0400

    Added windows + unix command for deleting BackingStore

--- kernel.cpp
+++ kernel.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <sstream>
 #include <fstream>
+#include <cstdlib>
 #include "shell.h"
 #include "ram.h"
 #include "cpu.h"
@@ -11,6 +12,7 @@ ReadyQueue queue;
 CPU cpu;
 
 void scheduler();
+void boot();
 
 void myInit(std::vector<std::string> &instructions) {
 
@@ -29,10 +31,10 @@ void myInit(std::vector<std::string> &instructions) {
     }
     scheduler();
 }
-//The ready queue works more like a queue rather than a linked list
+//The ready queue works more like a queue (hence the name) rather than a linked list
 //once the turn is up we pop it and move it to the back.
 //So to do this we pop the head then pass it to the cpu
-// then once the two quantas is up we pass it to the ready queue.
+//then once the two quantas is up we pass it to the ready queue.
 void scheduler() {
     while (!queue.isEmpty()) {
         auto ins = queue.instruction();
@@ -40,8 +42,24 @@ void scheduler() {
     }
 }
 
-int main() {
+void boot() {
 
+    //System command for Windows systems and Ubuntu Linux
+    //To remove the BackingStore Directory if it exists
+    //Both cases will silence the error output if folder does not exist
+    #ifdef _WIN32
+        std::system("if exist BackingStore rmdir /s /q Backingstore");
+    #else
+        std::system("rmdir BackingStore 2>/dev/null");
+    #endif
+
+    //Make the BackingStore
+    std::system("mkdir BackingStore");
+
+}
+
+int main() {
+    boot();
     std::cout << "Welcome to the C++ Shell\n";
     std::cout << "Version 1.1 Updated August 2024\n";
     std::cout << "Type 'help' to see an overview of available commands\n";
